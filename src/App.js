e8f5f6b1e71309a365ddf8343c5a9b304ee56e8a
karador <karadorkarador@gmail.com>
2020-07-17 12:36:48 +0300

    Add simple input with show text button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  // text переменная которая хранит данные, setText функция которая меняет text,
+  // никогда не меняй text напрямую! используй setText
+  // text = value неправильно
+  // setText(value) правильно
+  const [text, setText] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+        type="text"
+      />
+
+      <button onClick={() => alert(text)}>Show text</button>
     </div>
   );
 }
