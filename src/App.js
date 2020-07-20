7b199ef1c0b51d6c64fc238e92df36147b850d26
Alena <golovkoalena092@gmail.com>
2020-07-20 22:43:18 +0300

    Разделить приложение на несколько страниц

--- src/App.js
+++ src/App.js
@@ -3,14 +3,43 @@ import "./App.css";
 import Count from "./Count/Count";
 import Post from "./Post/Post";
 import UserList from "./UserList/UserList";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <Count />
-      <Post />
-      <UserList />
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Count</Link>
+          </li>
+          <li>
+            <Link to="/post">Post</Link>
+          </li>
+          <li>
+            <Link to="/userlist">UserList</Link>
+          </li>
+        </ul>
+        <hr/>
+        <Switch>
+          <Route exact path="/">
+            <Count/>
+          </Route>
+          <Route path="/post">
+            <Post/>
+          </Route>
+          <Route path="/userlist">
+            <UserList/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
