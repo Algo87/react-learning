d6421fd44233681ae5a498690ec5038ca594cf65
karador <karadorkarador@gmail.com>
2020-07-20 16:23:59 +0300

    change Count

--- src/Count/Count.js
+++ src/Count/Count.js
@@ -2,13 +2,24 @@ import React, { useState } from "react";
 
 function Count() {
   const [count, setCount] = useState(0);
-  const [countInput, setCountInput] = useState(0);
+  const [countInput, setCountInput] = useState("");
+
+  // function handleChangeNumberInp(e) {
+  //   if (e.target.value.match(/[^0-9]/g)) {
+  //     e.target.value = e.target.value.replace(/[^0-9]/g, "");
+  //   }
+  //   return setCountInput(e.target.value);
+  // }
+
+  // \d тоже самое что и [0-9], * это любое кол-во символов,
+  // ^ это начало строки, $ это конец
+  // Мы меняем значение только если это число, иначе ничего не делаем
+  // Код получается короче и понятнее, и мы не делаем лишних операций
 
   function handleChangeNumberInp(e) {
-    if (e.target.value.match(/[^0-9]/g)) {
-      e.target.value = e.target.value.replace(/[^0-9]/g, "");
+    if (/^\d*$/.test(e.target.value)) {
+      return setCountInput(e.target.value);
     }
-    return setCountInput(e.target.value);
   }
 
   return (
@@ -18,6 +29,7 @@ function Count() {
         <button onClick={() => setCount(count + 1)}>count + 1</button>
       </div>
       <div>
+        {/* Я добавил в input value={countInput} чтобы input был контролируемый */}
         <input onChange={handleChangeNumberInp} />
         <button onClick={() => setCount(count + +countInput)}>
           {" "}
