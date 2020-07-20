11ee2a9e61b8005458300c2d1cd0649ea9da7633
karador <karadorkarador@gmail.com>
2020-07-20 11:29:20 +0300

    move css to index.css

--- src/Count/Count.js
+++ src/Count/Count.js
@@ -1,29 +1,26 @@
-import React, {useState} from "react";
-import "./Count.css";
+import React, { useState } from "react";
 
 function Count() {
-    const [count, setCount] = useState(0);
-    const [countInput, setCountInput] = useState(0);
+  const [count, setCount] = useState(0);
+  const [countInput, setCountInput] = useState(0);
 
-    return (
-        <div className='b-count'>
-            <h3>Task 1</h3>
-            <div>
-                <button onClick={() => setCount(count + 1)}>
-                    count + 1
-                </button>
-            </div>
-            <div>
-                <input onChange={(e) => setCountInput(e.target.value)}/>
+  return (
+    <div className="b-count">
+      <h3>Task 1</h3>
+      <div>
+        <button onClick={() => setCount(count + 1)}>count + 1</button>
+      </div>
+      <div>
+        <input onChange={(e) => setCountInput(e.target.value)} />
 
-                <button onClick={() => setCount(count + (+countInput))}> count + inputValue</button>
-            </div>
-            <p className="result">Result: {count}</p>
-
-        </div>
-
-    )
-
+        <button onClick={() => setCount(count + +countInput)}>
+          {" "}
+          count + inputValue
+        </button>
+      </div>
+      <p className="result">Result: {count}</p>
+    </div>
+  );
 }
 
-export default Count
+export default Count;
--- src/UserList/UserList.js
+++ src/UserList/UserList.js
@@ -1,37 +1,33 @@
-import React, {useState, useEffect} from "react";
-import './UserList.css';
+import React, { useState, useEffect } from "react";
 
 function UserList() {
-    const [usersInfo, setUsersInfo] = useState([]);
-    const [load, setLoad] = useState(false);
-    useEffect(() => {
+  const [usersInfo, setUsersInfo] = useState([]);
+  const [load, setLoad] = useState(false);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        setLoad(true);
+        return setUsersInfo(json);
+      });
+  }, [load]);
 
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => {
-               return(response.json())
-            })
-            .then(json => {
-                setLoad(true);
-               return(setUsersInfo(json))
-            });
-
-    }, [load]);
-
-    return (
-        <div className="b-user-list">
-            <h3>Task 3</h3>
-            {load
-                ? <ul>
-                    {usersInfo.map(item => (
-                        <li key={item.id}>
-                            {item.name}
-                        </li>
-                    ))}
-                </ul>
-                : <p>Loading...</p>
-            }
-        </div>
-    )
+  return (
+    <div className="b-user-list">
+      <h3>Task 3</h3>
+      {load ? (
+        <ul>
+          {usersInfo.map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
 
 export default UserList;
