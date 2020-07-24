d91b7c5cf1a156de01f29632867b9ad4afab38e8
E.Golovko <webcom.024.otp@gmail.com>
2020-07-24 16:09:01 +0300

    На главной добавить кнопку show more которая отобразит ещё 10 постов

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,37 +1,42 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
-import { ThemeContext } from "../App";
+import React, {useState, useEffect, useContext} from "react";
+import {Link} from "react-router-dom";
+import {ThemeContext} from "../App";
 
-function Post(props) {
+function Post() {
   const [post, setPost] = useState("");
   const [load, setLoad] = useState(null);
   const theme = useContext(ThemeContext);
+  const [limitPosts, setLimitPosts] = useState(10);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts/")
       .then((response) => response.json())
       .then((json) => {
         setLoad(true);
-        let textArr = [];
-        for (let i = 0; i < 10; i++) {
-          textArr[i] = json[i];
-        }
-        return setPost(textArr);
+        return setPost([...json]);
       })
       .catch((error) => console.error(error));
-  }, [load]);
+  }, [load, limitPosts]);
   let posts = [...post];
 
   return (
     <div className={`b-post ${theme}`}>
       <h3>Task 2</h3>
-      {posts.map((item) => (
-        <div className="b-post-wrap" key={item.id}>
-          <h3>{item.title}</h3>
-          <p>{item.body}</p>
-          <Link to={'post/' + item.id}>post link</Link>
-        </div>
-      ))}
+      {posts.map((item, index) => {
+        let html;
+          (index < +limitPosts) ?
+            html= (
+              <div className="b-post-wrap" key={item.id}>
+                <h3>{item.id} {item.title}</h3>
+                <p>{item.body}</p>
+                <Link to={'post/' + item.id}>post link</Link>
+              </div>
+            ):
+            html=null;
+          return html;
+        }
+      )}
+      <button onClick={()=>{setLimitPosts(limitPosts + 10)}}>Show more</button>
     </div>
   );
 }
